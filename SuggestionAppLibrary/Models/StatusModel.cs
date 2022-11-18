7d3037317a226e4b5a93eb20ced376b967837b7e
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-11-18 19:47:07 +0700

    Development Update#4 - has created all models into ClassLibrary; - we have deleted nullable setting into project setting(library, blazorApp); - have added 2 new global usings for library.

---
+++ SuggestionAppLibrary/Models/StatusModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionAppLibrary.Models;
+
+public class StatusModel
+{
+   [BsonId]//this is identifier
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string StatusName { get; set; }
+   public string StatusDescription { get; set; }
+}
